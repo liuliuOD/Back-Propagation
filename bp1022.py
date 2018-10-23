fbb99c2b5ab306c19ebcf63d4adaf9cf23f93623
liuliuOD <44393673+liuliuOD@users.noreply.github.com>
2018-10-23 16:28:46 +0800

    1023

---
+++ bp1022.py
@@ -0,0 +1,10 @@
+class NeuralNetwork:
+	def __init__(self, inputNum, hiddenNum, outputNum, hiddenWeights=null, outputWeights):
+
+
+class Layer:
+	def __init__(self, neuronNum, weight=null):
+
+class Neuron:
+	def __init__(self, weight):
+		self.weight = weight
